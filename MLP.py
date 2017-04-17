764c3b60dc39629c71d8dd02cf36fb20b9b7ffef
Henry Qiu <henryqiu315@gmail.com>
2017-04-17 12:49:13 -0500

    first draft of MLP working...?  need to figure out evaluation

--- MLP.py
+++ MLP.py
@@ -3,38 +3,61 @@ from keras.layers import Dense, Activation,Embedding,Input,concatenate
 import numpy as np
 import keras.layers as layers
 
+def preprocess_data(users_matrix, items_matrix, interactions_matrix, batch_size):
+    if interactions_matrix.size % batch_size != 0:
+        print(str(interactions_matrix.size) + 'is not divisible by ' + str(batch_size))
+        raise StandardError
+    users = []
+    items = []
+    interactions = []
+    while True:
+        for user_idx, user in enumerate(users_matrix):
+            for item_idx, item in enumerate(items_matrix):
+                users.append(user)
+                items.append(item)
+                interactions.append(interactions_matrix[user_idx][item_idx])
+                if len(users) == batch_size:
+                    yield ({'user_input': np.array(users), 'item_input': np.array(items)},
+                           np.array(interactions))
+                    users = []
+                    items = []
+                    interactions = []
+
+
 num_predictive_factors = 8
+batch_size = 10
 # embedding size is 2 * num_predictive_factors if MLP is 3 layered
 
-# generate fake data
-num_users = 20
-num_items = 30
-dummy_user_data = np.zeros(num_users)
-dummy_item_data = np.zeros(num_items)
-dummy_item_data[np.random.randint(len(dummy_item_data) + 1)] = 1
-dummy_user_data[np.random.randint(len(dummy_user_data) + 1)] = 1
+# load data
+one_hot_users = np.load('one_hot_user.npy')
+one_hot_movies = np.load('one_hot_movies.npy')
+interaction_mx = np.load('interaction_mx.npy')
+
+#users, items, interactions = preprocess_data(one_hot_users,one_hot_movies,interaction_mx)
+
 
 #https://datascience.stackexchange.com/questions/13428/what-is-the-significance-of-model-merging-in-keras
-user_input = Input(shape=(num_users,))
-item_input = Input(shape=(num_items,))
-user_embed = Embedding(2, num_predictive_factors * 2, input_length=num_users)(user_input)
-item_embed = Embedding(2, num_predictive_factors * 2, input_length=num_items)(item_input)
+user_input = Input(shape=(len(one_hot_users),),name='user_input')
+item_input = Input(shape=(len(one_hot_movies),),name='item_input')
+#user_embed = Embedding(2, num_predictive_factors * 2, input_length=len(one_hot_users))(user_input)
+#item_embed = Embedding(2, num_predictive_factors * 2, input_length=len(one_hot_movies))(item_input)
+user_embed = Dense(num_predictive_factors * 2, activation='sigmoid')(user_input)
+item_embed = Dense(num_predictive_factors * 2, activation='sigmoid')(item_input)
 merged_embed = concatenate([user_embed, item_embed], axis=1)
 mlp_1 = Dense(32, activation='relu')(merged_embed)
 mlp_2 = Dense(16, activation='relu')(mlp_1)
 mlp_3 = Dense(8, activation='relu')(mlp_2)
-main_output = Dense(1, activation='relu')(mlp_3)
-model = Model(inputs=[user_input, item_input], output = merged_embed)
+main_output = Dense(1, activation='sigmoid',name='main_output')(mlp_3)
+
+model = Model(inputs=[user_input, item_input], output=main_output)
 model.compile(optimizer='Adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
+#model.fit([users, items], interactions,epochs=50, batch_size=32)
 
-#user_mlp = Sequential()
-#item_mlp = Sequential()
-
-#user_mlp.add(Embedding(2, num_predictive_factors * 2, input_length=num_users))
-#item_mlp.add(Embedding(2, num_predictive_factors * 2, input_length=num_items))
-#https://github.com/fchollet/keras/issues/3921
-#merged_embed = Merge([user_mlp, item_mlp], mode='concat', concat_axis=1)
-#concat_layer = Merge.Concatenate(axis=1)
-#merged_embed = concat_layer([user_mlp, item_mlp])
+model.fit_generator(preprocess_data(one_hot_users,one_hot_movies,interaction_mx, batch_size),
+                    steps_per_epoch=interaction_mx.size/batch_size,
+                    epochs=10,
+                    verbose=1)
+#result = model.predict_generator(preprocess_data(one_hot_users,one_hot_movies,interaction_mx, batch_size),
+#                        steps = 1)
