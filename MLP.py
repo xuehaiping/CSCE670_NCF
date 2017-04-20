e4338257036495e157809b47bf091ba8a4932fba
xuehaiping <claymoresq@gmail.com>
2017-04-20 11:48:29 -0500

    added name to each mlp layer

--- MLP.py
+++ MLP.py
@@ -25,7 +25,7 @@ def preprocess_data(users_matrix, items_matrix, interactions_matrix, batch_size)
 
 
 num_predictive_factors = 8
-batch_size = 765
+batch_size = 1
 # embedding size is 2 * num_predictive_factors if MLP is 3 layered
 
 # load data
@@ -41,12 +41,12 @@ user_input = Input(shape=(len(one_hot_users),),name='user_input')
 item_input = Input(shape=(len(one_hot_movies),),name='item_input')
 #user_embed = Embedding(2, num_predictive_factors * 2, input_length=len(one_hot_users))(user_input)
 #item_embed = Embedding(2, num_predictive_factors * 2, input_length=len(one_hot_movies))(item_input)
-user_embed = Dense(num_predictive_factors * 2, activation='sigmoid')(user_input)
-item_embed = Dense(num_predictive_factors * 2, activation='sigmoid')(item_input)
+user_embed = Dense(num_predictive_factors * 2, activation='sigmoid',name='MLP_user_embed')(user_input)
+item_embed = Dense(num_predictive_factors * 2, activation='sigmoid',name='MLP_item_embed')(item_input)
 merged_embed = concatenate([user_embed, item_embed], axis=1)
-mlp_1 = Dense(32, activation='relu')(merged_embed)
-mlp_2 = Dense(16, activation='relu')(mlp_1)
-mlp_3 = Dense(8, activation='relu')(mlp_2)
+mlp_1 = Dense(32, activation='relu', name='mlp_1')(merged_embed)
+mlp_2 = Dense(16, activation='relu', name='mlp_2')(mlp_1)
+mlp_3 = Dense(8, activation='relu', name='mlp_3')(mlp_2)
 main_output = Dense(1, activation='sigmoid',name='main_output')(mlp_3)
 
 model = Model(inputs=[user_input, item_input], output=main_output)
@@ -62,5 +62,4 @@ model.fit_generator(preprocess_data(one_hot_users,one_hot_movies,interaction_mx,
 #result = model.predict_generator(preprocess_data(one_hot_users,one_hot_movies,interaction_mx, batch_size),
 #                        steps = 1)
 
-model.save('MLP.h5')
 
