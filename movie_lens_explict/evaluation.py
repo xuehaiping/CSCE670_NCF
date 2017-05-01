dde7bd135a17a7a99400e19d7764573928e18044
Roger <roger.solis@tamu.edu>
2017-04-30 20:21:52 -0500

    Fixed the retrieval of testing data

--- movie_lens_explict/evaluation.py
+++ movie_lens_explict/evaluation.py
@@ -3,7 +3,6 @@ import operator, data_management
 from math import log
 from operator import div
 
-NDCG_SCORES = np.arange(1,0,-0.1)
 def hit_rate(sorted_predictions, target_movie, target_rating):
     movies = [int(i[0]) for i in sorted_predictions]
     #ratings = [int(i[1]) for i in sorted_predictions]
@@ -29,58 +28,59 @@ def evaluate_rmse(model):
     return rmse
 
 def evaluate_integer_input(fname, model, metric, interactions_matrix):
-    target_movies = []
-    target_ratings = []
+    target_movies = {}
+    target_ratings = {}
     int_matrix = np.load(interactions_matrix)
     int_matrix = np.delete(int_matrix, 0, 0)
-    int_matrix = np.delete(int_matrix,0,1)
-    
+    int_matrix = np.delete(int_matrix, 0, 1)
+
     lines = np.load(fname)
-    
+
     for line in lines:
-        target_movies.append(line[1])
-        target_ratings.append(line[2])
-    
+        
+        if line[0] not in target_movies.keys(): 
+            target_movies[line[0]] = [line[1]]
+            target_ratings[line[0]] = [line[2]]
+        else:
+            target_movies[line[0]].append(line[1])
+            target_ratings[line[0]].append(line[2])
+
     summation = 0
-    #int_matrix == [[ 0.  0.  1. ...,  0.  0.  4.]]
-    
+    # int_matrix == [[ 0.  0.  1. ...,  0.  0.  4.]]
+
     for idx, user in enumerate(int_matrix):
-        #User is a row build from movie's ratings (rather than only 1's)
-        
-        # pick 100 random non-rated movies 
-        #zero_indices = np.where(user == 0)[0]
-        #np.random.shuffle(zero_indices)
-        #random_indices = zero_indices[0:100] #Movie Ids
-        
+        # User is a row build from movie's ratings (rather than only 1's)
         # transform into inputs for keras
-        movie_vectors = target_movies[idx]
-        rating_vectors = target_ratings[idx]
-        user_vectors = np.repeat([idx + 1], rating_vectors.size, axis=0)
         
-        #Sort movies by rating in decreasing order. So movie_vectors, rating_vectors and user_vectors
-        movie_rating_user_tuples = [(movie_vectors[i], rating_vectors[i], user_vectors[i]) for i,v in enumerate(movie_vectors)]
+        movie_vectors = target_movies[idx + 1]
+        rating_vectors = target_ratings[idx + 1]
+        user_vectors = np.repeat([idx + 1], len(rating_vectors), axis=0)        
+        
+        # Sort movies by rating in decreasing order. So movie_vectors, rating_vectors and user_vectors
+        movie_rating_user_tuples = [(movie_vectors[i], rating_vectors[i], user_vectors[i]) for i, v in
+                                    enumerate(movie_vectors)]
         sorted_tuples = sorted(movie_rating_user_tuples, key=lambda item: item[1])
-        
-        #Put them back to arrays so we can pass to the NDCG function an ideal ratings vector
-        #We do this right before predict, so the predicted labels are returned in this same way, so we only need to pass it to ndcg
-        for i,v in enumerate(sorted_tuples):
+
+        # Put them back to arrays so we can pass to the NDCG function an ideal ratings vector
+        # We do this right before predict, so the predicted labels are returned in this same way, so we only need to pass it to ndcg
+        for i, v in enumerate(sorted_tuples):
             movie_vectors[i] = v[0]
             rating_vectors[i] = v[1]
             user_vectors[i] = v[2]
-        
+
         # generate predictions. This predictions are in the same order as movie_vectors, so we can pass it as it is to the ndcg function
         predictions = model.predict({'user_input': np.array(user_vectors), 'item_input': np.array(movie_vectors)})
-        #predictions_idx = dict(zip(movie_vectors, predictions))
-        
-        #Sorted by ratings
-        #sorted_predictions = sorted(predictions_idx.items(), key=operator.itemgetter(1), reverse= True)[0:5]
+        # predictions_idx = dict(zip(movie_vectors, predictions))
+
+        # Sorted by ratings
+        # sorted_predictions = sorted(predictions_idx.items(), key=operator.itemgetter(1), reverse= True)[0:5]
         
         if metric == 'hit_rate':
             raise StandardError('Hit rate not suported for rankings"')
-            #if hit_rate(sorted_predictions, target_movies[idx], target_ratings[idx]):
-                #summation += 1
+            # if hit_rate(sorted_predictions, target_movies[idx], target_ratings[idx]):
+            # summation += 1
         elif metric == 'ndcg':
-            return ndcg(rating_vectors, predictions)
+            summation += ndcg(target_ratings[idx + 1], predictions)
         else:
             raise StandardError('metric has to be "ndcg"')
-    return summation/float(int_matrix.shape[0])
+    return summation / float(int_matrix.shape[0])
